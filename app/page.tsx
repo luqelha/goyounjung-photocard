920b62672f09f90b2c019e04f820719db0cf1e5d
luqelha <ytmlhg@gmail.com>
2026-07-20 21:04:17 +0700

    dunno

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,15 @@
-import { Navbar } from '@/components/layout/Navbar';
-import { Footer } from '@/components/layout/Footer';
-import { HeroSection } from '@/components/sections/HeroSection';
-import { HighlightPerformance } from '@/components/sections/HighlightPerformance';
-import { FilmographySection } from '@/components/sections/FilmographySection';
-import { AboutSection } from '@/components/sections/AboutSection';
-import { AwardsMarquee } from '@/components/sections/AwardsMarquee';
-import { EditorialGallery } from '@/components/sections/EditorialGallery';
+import { Navbar } from "@/components/layout/Navbar";
+import { Footer } from "@/components/layout/Footer";
+import { HeroSection } from "@/components/sections/HeroSection";
+import { HighlightPerformance } from "@/components/sections/HighlightPerformance";
+import { FilmographySection } from "@/components/sections/FilmographySection";
+import { AboutSection } from "@/components/sections/AboutSection";
+import { AwardsMarquee } from "@/components/sections/AwardsMarquee";
+import { EditorialGallery } from "@/components/sections/EditorialGallery";
 
 export default function Home() {
   return (
-    <main className='relative bg-background min-h-screen'>
+    <main className="relative bg-background min-h-screen">
       <Navbar />
       <HeroSection />
       <AboutSection />
